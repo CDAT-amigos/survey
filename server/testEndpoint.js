05bbf49f0ee42668947511f8d7143e1281efd7c9
Daniel Stahl <danstahl1138@gmail.com>
2018-08-17 17:57:46 -0500

    works!

--- server/testEndpoint.js
+++ server/testEndpoint.js
@@ -1,8 +1,6 @@
 const {GraphQLClient} = require('graphql-request')
 require('dotenv').config()
 const {API_KEY}=process.env
-console.log(API_KEY)
-
 const query = `{
   listUsers{
     items{
@@ -18,7 +16,7 @@ const client= new GraphQLClient(
     'https://finozlhfpjgh5a6lzt2yefvwoi.appsync-api.us-east-2.amazonaws.com/graphql', 
     {
         headers:{
-            Authorization:API_KEY
+          "x-api-key":API_KEY
         }
     }
 )
